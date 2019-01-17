caad6ec4ce42f12b0cf515c4f5eac04f22063dd5
dlaghks93 <ghks93@nate.com>
2019-01-17 15:19:19 +0900

    문제

---
+++ 문제/problem2.playground/Contents.swift
@@ -0,0 +1,8 @@
+func helloworld() -> String{
+    return ""
+}
+
+let result = helloworld()
+assert(result == "Hello World!", "Test Fail")
+
+print("정답")
---
+++ 문제/problem3.playground/Contents.swift
@@ -0,0 +1,4 @@
+// 아래 제시된 총 과일의 종류 수 보다 많은 과일 종류와 갯수만 출력하여라.(20)
+
+var fruits : [String:Int] = ["Apple" : 4, "Banana" : 3, "Pear" : 9, "Strawberry" : 20, "Lime" : 6, "Peach" : 14, "Orange" : 2]
+
